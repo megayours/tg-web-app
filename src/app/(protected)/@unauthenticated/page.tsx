c9a946eb3da5ad97c6391f6d47e8fcd80eca1940
Viktor Plane <viktor@planevin.se>
2024-11-09 18:38:25 +0100

    fix: remove megayours mention

--- src/app/(protected)/@unauthenticated/page.tsx
+++ src/app/(protected)/@unauthenticated/page.tsx
@@ -1,8 +1,7 @@
 import UserButtons from "@/libs/chromia-connect/user-buttons";
-import { Section, Text } from "@telegram-apps/telegram-ui";
+import { Section } from "@telegram-apps/telegram-ui";
 import "./styles.css";
 import Logo from "@/components/Logo/Logo";
-import Link from "next/link";
 
 const Unauthenticated = () => (
   <Section className="unauthenticated_container">
@@ -16,51 +15,6 @@ const Unauthenticated = () => (
           <UserButtons />
         </div>
       </div>
-
-      <div className="unauthenticated_features">
-        <div className="unauthenticated_feature">
-          <Text Component="span" style={{ fontWeight: 'bold', marginBottom: '2px' }}>
-            🧩 Composable
-          </Text>
-          <Text Component="span">
-            Mix & match token features
-          </Text>
-        </div>
-
-        <div className="unauthenticated_feature">
-          <Text Component="span" style={{ fontWeight: 'bold', marginBottom: '4px' }}>
-            🎨 Dynamic
-          </Text>
-          <Text Component="span">
-            Remixable metadata
-          </Text>
-        </div>
-
-        <div className="unauthenticated_feature">
-          <Text Component="span" style={{ fontWeight: 'bold', marginBottom: '4px' }}>
-            ⛓️ <br/>On-chain
-          </Text>
-          <Text Component="span">
-            Metadata on the blockchain
-          </Text>
-        </div>
-
-        <div className="unauthenticated_feature">
-          <Text Component="span" style={{ fontWeight: 'bold', marginBottom: '4px' }}>
-            🌐 Decentralized
-          </Text>
-          <Text Component="span">
-            True ownership
-          </Text>
-        </div>
-      </div>
-    </div>
-
-    <div className="unauthenticated_footer">
-      <Text Component="span" style={{ color: 'var(--tg-theme-hint-color)' }}>
-        Copyright {new Date().getFullYear()}
-      </Text>
-      <Link href="https://megayours.com" target="_blank">megayours.com</Link>
     </div>
   </Section>
 );
